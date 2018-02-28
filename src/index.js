9c56ba9a21170d89e42e49f261d83e8ebdf0303a
VLanc <vlancv@gmail.com>
2018-02-28 14:46:08 +0300

    Task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let inputStrArr = str.split("");
+  let bracketsArr = [];
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    for(let j = 0; j < bracketsConfig[i].length; j++) {
+      bracketsArr.push(bracketsConfig[i][j]);
+    }
+  }
+
+  for(let i = 0; i < inputStrArr.length; i++) {
+    for(let j = 0; j < bracketsArr.length; j += 2) {
+      if(inputStrArr[i] == bracketsArr[j] && inputStrArr[i+1] == bracketsArr[j+1]) {
+        inputStrArr.splice(i, 2); // will delete opening-closing brackets pairs while they're
+        i = -1;
+        break;
+      }
+    }
+  }
+
+  if(inputStrArr[0]) {
+    return false;
+  } else return true;
 }
